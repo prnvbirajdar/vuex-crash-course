0ef50b31bc3fdd729292119cc72a8e2edadd1966
Pranav Birajdar <prnvbirajdar@gmail.com>
2021-06-05 13:59:39 -0400

    added counter

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,11 +1,25 @@
 <template>
-  <div>
-    counter
-  </div>
+  <h1>{{ counter }}</h1>
+  <h2>{{ inputVal }}</h2>
+  <button>-</button>
+  <input type="number" v-model="inputVal" />
+  <button @click="increment">+</button>
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const counter = ref(0);
+
+    const inputVal = ref(0);
+
+    const increment = () => counter.value++;
+
+    return { counter, inputVal, increment };
+  },
+};
 </script>
 
 <style scoped></style>
